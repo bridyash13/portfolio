3e4b84e841474feadcd6da9d35cbb396cb7d841f
bridyash13 <bridyash31@gmail.com>
2022-04-10 20:25:12 +0530

    Changed chat button

--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -5,7 +5,6 @@ const CTA = () => {
   return (
     <div className='cta'>
     <a href={cv} download className='btn'>Download CV</a>
-    <a href="#contact" className='btn btn-primary'>Lets Talk</a>
     </div>
   )
 }
